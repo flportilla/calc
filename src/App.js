66c267e840a66bb7fd72d61c3a4ed93e4fc29463
flportilla <flportilla@gmail.com>
2022-06-14 18:00:18 -0500

    Add logo and logo css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import '../src/styles/App.css';
+import freeCodeCampLogo from '../src/img/freecodecamp-logo.png';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <div className='freecodecamp_logo_container'>
+        <img
+          src={freeCodeCampLogo}
+          alt='logo de freeCodeCamp'
+          className='freecodecamp_logo'
+        />
+      </div>
     </div>
   );
 }
