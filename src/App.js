8328e167062fb5f17028d44038f77af7d905b06b
flportilla <flportilla@gmail.com>
2022-06-14 18:40:07 -0500

    Add screen and css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import '../src/styles/App.css';
 import freeCodeCampLogo from '../src/img/freecodecamp-logo.png';
 import Button from './components/Button';
+import Screen from './components/Screen';
 
 function App() {
   return (
@@ -12,14 +13,37 @@ function App() {
           className='freecodecamp_logo'
         />
       </div>
+
       <div className='calc_container'>
+        <Screen />
         <div className='row'>
           <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
           <Button>+</Button>
         </div>
-        <div className='row'></div>
-        <div className='row'></div>
-        <div className='row'></div>
+
+        <div className='row'>
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>-</Button>
+        </div>
+
+        <div className='row'>
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>*</Button>
+        </div>
+
+        <div className='row'>
+          <Button>=</Button>
+          <Button>0</Button>
+          <Button>.</Button>
+          <Button>/</Button>
+        </div>
+
         <div className='row'></div>
       </div>
     </div>
---
+++ src/components/Screen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Screen.css'
+
+function Screen() {
+  return (
+    <div
+      className='input'
+    >
+    </div>
+  )
+}
+
+export default Screen
